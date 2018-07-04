bd94a7fad70e59145b3dfc74544beac673db0079
Steven Murphy <steve231@gmail.com>
2018-07-04 20:53:18 +0100

    Restore spring shell starter code

--- stockbroker/src/main/java/com/rook12/stockbroker/commands/StockBrokerCommands.java
+++ stockbroker/src/main/java/com/rook12/stockbroker/commands/StockBrokerCommands.java
@@ -6,37 +6,30 @@ import com.rook12.stockbroker.dto.DividendYieldResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.shell.core.CommandMarker;
-import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
-import org.springframework.shell.core.annotation.CliCommand;
-import org.springframework.stereotype.Component;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
-@Component
-public class StockBrokerCommands implements CommandMarker {
+@ShellComponent
+public class StockBrokerCommands {
 
     private StockExchangeInterface exchangeInterface;
 
     private static final Logger logger = LoggerFactory.getLogger(StockBrokerCommands.class);
 
-    /*@Autowired
+    @Autowired
     public StockBrokerCommands(StockExchangeInterface exchangeInterface) {
         this.exchangeInterface = exchangeInterface;
-    }*/
-
-    @CliAvailabilityIndicator(value="hello")
-    public boolean isPrintCommandAvailable()
-    {
-        return Boolean.TRUE;
     }
 
-    @CliCommand(value={"hello","hi"})
+    @ShellMethod("Say hello")
     public String hello() {
 
         logger.info("executing hello");
         return "hello";
     }
 
-    //@ShellMethod("Calculate dividend yield")
+    @ShellMethod("Calculate dividend yield")
     public String calculateDividendYield(String stockSymbol, int marketPrice) {
         logger.info(String.format("calculating dividend yield for %s at price %s", stockSymbol, marketPrice));
         DividendYieldResponse dividendYieldResponse = exchangeInterface.getDividendYield(stockSymbol, marketPrice);
@@ -44,22 +37,22 @@ public class StockBrokerCommands implements CommandMarker {
         return dividendYieldResponse.getDividendYield().toString();
     }
 
-   // @ShellMethod("Calculate P/E ratio")
+    @ShellMethod("Calculate P/E ratio")
     public String calculatePERatio(String stockSymbol, int marketPrice) {
         return "";
     }
 
-    //@ShellMethod("Execute trade")
+    @ShellMethod("Execute trade")
     public String executeTrade(String stockSymbol, int quantity, OrderAction orderAction, int tradePrice) {
         return orderAction.toString();
     }
 
-   // @ShellMethod("Calculate Volume Weighted Stock Price")
-    /*public String calculateVWSP(String stockSymbol, @ShellOption(defaultValue = "15") int timePeriod) {
+    @ShellMethod("Calculate Volume Weighted Stock Price")
+    public String calculateVWSP(String stockSymbol, @ShellOption(defaultValue = "15") int timePeriod) {
         return Integer.toString(timePeriod);
-    }*/
+    }
 
-    //@ShellMethod("Calculate all share index")
+    @ShellMethod("Calculate all share index")
     public String calculateAllShareIndex() {
         return "";
     }
