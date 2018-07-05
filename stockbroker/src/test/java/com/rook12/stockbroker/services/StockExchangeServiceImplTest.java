7f3f11f4f33cb18c43dc0fe0bc8e4c26cb3df59d
Steven Murphy <steve231@gmail.com>
2018-07-05 18:59:29 +0100

    Bring Jackson versions into sync. Mock up dividend yield web service call test.

--- stockbroker/src/test/java/com/rook12/stockbroker/services/StockExchangeServiceImplTest.java
+++ stockbroker/src/test/java/com/rook12/stockbroker/services/StockExchangeServiceImplTest.java
@@ -1,14 +1,19 @@
 package com.rook12.stockbroker.services;
 
+import com.rook12.stockbroker.dto.DividendYieldResponse;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
+
+import static com.sun.javaws.JnlpxArgs.verify;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.client.ExpectedCount.manyTimes;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
@@ -25,11 +30,18 @@ public class StockExchangeServiceImplTest {
 
     @Test
     public void getDividendYield() {
-        mockRestServiceServer.expect(manyTimes(), requestTo(""))
+        String dividendJson = "{\"dividendYield\":12.34,\n" +
+                "  \"stockType\":\"COMMON\",\n" +
+                "  \"stockSymbol\":\"POP\",\n" +
+                "  \"marketPrice\":100" +
+                "}";
+        mockRestServiceServer.expect(manyTimes(), requestTo("http://localhost:8080/api/calculateDividendYield?stockSymbol=POP&marketPrice=100"))
                 .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess("", MediaType.APPLICATION_JSON));
+                .andRespond(withSuccess(dividendJson, MediaType.APPLICATION_JSON_UTF8));
 
-        stockExchangeService.getDividendYield("POP", 100);
+        DividendYieldResponse response = stockExchangeService.getDividendYield("POP", 100);
+
+        assertEquals(new BigDecimal("12.34"), response.getDividendYield());
     }
 
     @Test
